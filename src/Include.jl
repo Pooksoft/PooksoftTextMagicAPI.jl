28c74ff670aef7c7aadbe05cde110acb537a6fed
Jeffrey Varner <jvarner@pooksoft.com>
2021-01-29 15:19:23 -0500

    Update Include.jl
    updates

--- src/Include.jl
+++ src/Include.jl
@@ -16,7 +16,7 @@ using FileWatching
 using Logging
 
 # load my codes -
-include(joinpath(_PATH_TO_SRC,"Types.jl"))
-include(joinpath(_PATH_TO_SRC,"Files.jl"))
-include(joinpath(_PATH_TO_SRC,"Factory.jl"))
-include(joinpath(_PATH_TO_SRC,"Network.jl"))
+include("./Types.jl")
+include("./Files.jl")
+include("./Factory.jl")
+include("./Network.jl")
